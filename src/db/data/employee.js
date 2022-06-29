553ca98cdc31d4b957842893cec723ffa3740716
Tanveer Bassi <tanveer13a551@gmail.com>
2022-06-29 17:49:47 +0100

    updated databases

---
+++ src/db/data/employee.js
@@ -0,0 +1,34 @@
+const employee = [
+  {
+    first_name: "Adam",
+    last_name: "West",
+    role_id: "2",
+    manager_id: "2",
+  },
+  {
+    first_name: "Tom",
+    last_name: "Hardy",
+    role_id: "3",
+    manager_id: "4",
+  },
+  {
+    first_name: "Adam",
+    last_name: "Sandler",
+    role_id: "1",
+    manager_id: "5",
+  },
+  {
+    first_name: "Morgan",
+    last_name: "Freeman",
+    role_id: "6",
+    manager_id: "2",
+  },
+  {
+    first_name: "Mark",
+    last_name: "Harris",
+    role_id: "2",
+    manager_id: "2",
+  },
+];
+
+module.exports = employee;
